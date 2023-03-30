7658aca9c73a9803ac0e6a7d6c8837278bc91f2a
certh1 <sightspotify@gmail.com>
2023-03-30 19:58:43 +0700

    Update PlacementGenerator.cs

--- Assets/Scripts/World Generation/PlacementGenerator.cs
+++ Assets/Scripts/World Generation/PlacementGenerator.cs
@@ -1,18 +1,33 @@
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+[System.Serializable]
+public struct PrefabSettings {
+    public GameObject prefab;
+    
+    public int density;
+    public float minHeight;
+    public float maxHeight;
+    public int maxPrefabs;
+}
 
 public class PlacementGenerator : MonoBehaviour
 {
     [SerializeField] List<GameObject> prefabs;
+    public PrefabSettings[] settingsList;
 
     [Header("Raycast Settings")]
     [SerializeField] int density;
 
     [Space]
 
-    [SerializeField] float minHeight;
-    [SerializeField] float maxHeight;
     [SerializeField] Vector2 xRange;
     [SerializeField] Vector2 zRange;
 
@@ -22,37 +37,55 @@ public class PlacementGenerator : MonoBehaviour
     [SerializeField] Vector3 minScale;
     [SerializeField] Vector3 maxScale;
 
+    private void Start()
+    {
+        settingsList = new PrefabSettings[prefabs.Count];
+        for (int i = 0; i < prefabs.Count; ++i)
+        {
+            settingsList[i] = new PrefabSettings();
+            settingsList[i].prefab = prefabs[i];
+        }
+    }
+
 #if UNITY_EDITOR
     public void Generate()
     {
         Clear();
-
-        for (int i = 0; i < density; i++)
+        for (int prefab_i = 0; prefab_i < prefabs.Count; prefab_i++)
         {
-            int prefabIndex = Random.Range(0, prefabs.Count);
-            GameObject prefab = prefabs[prefabIndex];
+            GameObject prefab = prefabs[prefab_i];
+            PrefabSettings currentSettingList = settingsList[prefab_i];
+            for (int i = 0; i < currentSettingList.density; ++i)
+            {
+                float minHeight = currentSettingList.minHeight;
+                float maxHeight = currentSettingList.maxHeight;
 
-            float sampleX = Random.Range(xRange.x, xRange.y);
-            float sampleY = Random.Range(zRange.x, zRange.y);
-            Vector3 rayStart = new(sampleX, maxHeight, sampleY);
+                Debug.Log(string.Format("CurrentIndex: {0} | Density: {1} | CurrentLoopI: {2} | MinHeight: {3} | MaxHeight: {4}", prefab.name, currentSettingList.density, i, minHeight, maxHeight));
 
-            if (!Physics.Raycast(rayStart, Vector3.down, out RaycastHit hit, Mathf.Infinity))
-                continue;
+                float sampleX = Random.Range(xRange.x, xRange.y);
+                float sampleY = Random.Range(zRange.x, zRange.y);
+                Vector3 rayStart = new(sampleX, maxHeight, sampleY);
 
-            if (hit.point.y < minHeight)
-                continue;
+                if (!Physics.Raycast(rayStart, Vector3.down, out RaycastHit hit, Mathf.Infinity))
+                    continue;
 
-            GameObject instantiatedPrefab = (GameObject)PrefabUtility.InstantiatePrefab(prefab, transform);
+                if (hit.point.y < minHeight)
+                    continue;
 
-            instantiatedPrefab.transform.SetParent(transform);
-            instantiatedPrefab.transform.position = hit.point;
-            instantiatedPrefab.transform.Rotate(Vector3.up, Random.Range(rotationRange.x, rotationRange.y), Space.Self);
-            instantiatedPrefab.transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation * Quaternion.FromToRotation(instantiatedPrefab.transform.up, hit.normal), rotateTowardsNormal);
-            instantiatedPrefab.transform.localScale = new Vector3(
-                Random.Range(minScale.x, maxScale.x),
-                Random.Range(minScale.y, maxScale.y),
-                Random.Range(minScale.z, maxScale.z)
-            );
+                GameObject instantiatedPrefab = (GameObject)PrefabUtility.InstantiatePrefab(prefab, transform);
+
+                instantiatedPrefab.transform.SetParent(transform);
+                instantiatedPrefab.transform.position = hit.point;
+                instantiatedPrefab.transform.Rotate(Vector3.up, Random.Range(rotationRange.x, rotationRange.y), Space.Self);
+                instantiatedPrefab.transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation * Quaternion.FromToRotation(instantiatedPrefab.transform.up, hit.normal), rotateTowardsNormal);
+                instantiatedPrefab.transform.localScale = new Vector3(
+                    Random.Range(minScale.x, maxScale.x),
+                    Random.Range(minScale.y, maxScale.y),
+                    Random.Range(minScale.z, maxScale.z)
+                );
+
+                currentSettingList.maxPrefabs++;
+            }
         }
     }
 
@@ -66,3 +99,4 @@ public class PlacementGenerator : MonoBehaviour
 
 #endif
 }
+
